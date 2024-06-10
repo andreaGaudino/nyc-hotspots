3067e82828423f73c37dddcd0efc6ca501d31fdd
andreaGaudino <andrea.gaudino02@gmail.com>
2024-06-10 09:17:12 +0200

    inizio

--- UI/view.py
+++ UI/view.py
@@ -6,9 +6,9 @@ class View(ft.UserControl):
         super().__init__()
         # page stuff
         self._page = page
-        self._page.title = "Template application using MVC and DAO"
+        self._page.title = "NYC Wi-Fi hotspot"
         self._page.horizontal_alignment = 'CENTER'
-        self._page.theme_mode = ft.ThemeMode.DARK
+        self._page.theme_mode = ft.ThemeMode.LIGHT
         # controller (it is not initialized. Must be initialized in the main, after the controller is created)
         self._controller = None
         # graphical elements
@@ -20,27 +20,38 @@ class View(ft.UserControl):
 
     def load_interface(self):
         # title
-        self._title = ft.Text("Hello World", color="blue", size=24)
-        self._page.controls.append(self._title)
+        # self._title = ft.Text("Hello World", color="blue", size=24)
+        self._page.controls.append(ft.Text("NYC Wi-Fi hotspot", color="blue", size=24))
 
-        #ROW with some controls
-        # text field for the name
-        self.txt_name = ft.TextField(
-            label="name",
-            width=200,
-            hint_text="Insert a your name"
-        )
-
-        # button for the "hello" reply
-        self.btn_hello = ft.ElevatedButton(text="Hello", on_click=self._controller.handle_hello)
-        row1 = ft.Row([self.txt_name, self.btn_hello],
-                      alignment=ft.MainAxisAlignment.CENTER)
+        #row 1
+        self.ddProvider = ft.Dropdown(label="Provider")
+        self.btnCreaGrafo = ft.ElevatedButton(text="Crea grafo", on_click=self._controller.handleCreaGrafo)
+        row1 = ft.Row([ft.Container(self.ddProvider, width=300), ft.Container(self.btnCreaGrafo, width=200)], alignment=ft.MainAxisAlignment.CENTER)
         self._page.controls.append(row1)
 
-        # List View where the reply is printed
-        self.txt_result = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
-        self._page.controls.append(self.txt_result)
-        self._page.update()
+        #row 2
+        self.txtInDistanza = ft.TextField(label="Distanza")
+        self.btnAnalisiGrafo = ft.ElevatedButton(text="Analisi grafo", on_click=self._controller.handleAnalisiGrafo)
+        row2 = ft.Row([
+            ft.Container(self.txtInDistanza, width=300),
+            ft.Container(self.btnAnalisiGrafo, width=200)], alignment=ft.MainAxisAlignment.CENTER)
+
+        self._page.controls.append(row2)
+
+        #row 3
+        self.txtInString = ft.TextField(label="Stringa")
+        self.btnCalcolaPercorso = ft.ElevatedButton(text="Calcola percorso", on_click=self._controller.handleCalcolaPercorso)
+        row3 = ft.Row([ft.Container(self.txtInString, width=300),
+                       ft.Container(self.btnCalcolaPercorso, width=200)], alignment=ft.MainAxisAlignment.CENTER)
+
+        self._page.controls.append(row3)
+
+        #row4
+        self.ddTarget = ft.Dropdown(label='Target')
+        row4 = ft.Row([ft.Container(self.ddTarget, width=300), ft.Container(None, width=200)], alignment=ft.MainAxisAlignment.CENTER)
+        self._page.controls.append(row4)
+
+        self.update_page()
 
     @property
     def controller(self):
--- database/DAO.py
+++ database/DAO.py
@@ -4,3 +4,23 @@ from database.DB_connect import DBConnect
 class DAO():
     def __init__(self):
         pass
+
+    @staticmethod
+    def getAllProviders():
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """select a.* , sum(t.Milliseconds) as totD
+                        from album a , track t 
+                        where a.AlbumId = t.AlbumId 
+                        group by a.AlbumId 
+                        having totD > %s """
+        cursor.execute(query, (d,))
+
+        for row in cursor:
+            result.append(Album(**row))
+        cursor.close()
+        conn.close()
+        return result
